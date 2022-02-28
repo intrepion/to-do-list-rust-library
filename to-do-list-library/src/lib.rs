0aa33a55ffc9f97064d165235e9355ac51826e0e
Oliver Forral <intrepion@gmail.com>
2022-02-27 22:33:04 -0700

    green

--- to-do-list-library/src/lib.rs
+++ to-do-list-library/src/lib.rs
@@ -1,9 +1,19 @@
 #[cfg(test)]
 mod tests {
     mod to_do_item {
+        use super::super::ToDoItem;
+
         #[test]
         fn default_should_create_new_to_do_item() {
-            let to_do_item = ToDoItem::default();
+            let _to_do_item = ToDoItem::default();
         }
+    }
+}
+
+pub struct ToDoItem {}
+
+impl ToDoItem {
+    pub fn default() -> ToDoItem {
+        ToDoItem {}
     }
 }
