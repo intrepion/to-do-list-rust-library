2b7baea328e63276665413364bd92dfcbff8dd85
Oliver Forral <intrepion@gmail.com>
2022-02-27 22:34:13 -0700

    red

--- to-do-list-library/src/lib.rs
+++ to-do-list-library/src/lib.rs
@@ -5,7 +5,10 @@ mod tests {
 
         #[test]
         fn default_should_create_new_to_do_item() {
-            let _to_do_item = ToDoItem::default();
+            let to_do_item = ToDoItem::default();
+
+            let _expected = "Edit this to do item.".to_owned();
+            let actual = to_do_item.get_title();
         }
     }
 }
